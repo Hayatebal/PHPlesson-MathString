6d790839db54452eb327cceff94bd50113c331c8
Marian Balila-on <hayatebal@gmail.com>
2026-02-25 09:59:46 +0800

    Add files via upload

---
+++ typesofarray.php
@@ -0,0 +1,23 @@
+<?php
+
+$fruits = array("Apple","Banana","Mango") ;
+
+echo $fruits[0];
+echo $fruits[1];
+
+$student = array("name" => "Reynard","age" => 20,"course" => "IT");
+
+echo "Name: ", $student["name"],"<br>";
+echo "Age: ", $student["age"],"<br>";
+echo "Course: ", $student["course"],"<br>";
+
+$students = array{
+   array("name" => "Reynard","age" => 20,"course" => "IT");
+   array("name" => "Anna","age" => 21,"course" => "CS");
+   array("name" => "John","age" => 25,"course" => "GameDev");
+}
+
+?>
+
+
+
